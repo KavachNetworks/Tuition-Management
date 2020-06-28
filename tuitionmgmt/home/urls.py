4e81c17d29448507a60283068c76cd529670c939
Vinayak Jaiwant <vjaiwantx@gmail.com>
2020-06-28 09:50:41 +0530

    added navigation (simple)

--- tuitionmgmt/home/urls.py
+++ tuitionmgmt/home/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.home)
+    path('',views.home,name='home'),
+    path('contact',views.contact_us,name="contactus"),
+    path('signup',views.signin,name='signup'),
+    path('signin',views.signup,name='signin')
 ]
