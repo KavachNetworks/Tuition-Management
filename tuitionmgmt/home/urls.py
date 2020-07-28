68349c6ed6c399d937255cd7df2112323c4de805
Vinayak Jaiwant <vjaiwantx@gmail.com>
2020-07-28 23:22:38 +0530

    Add Changes

--- tuitionmgmt/home/urls.py
+++ tuitionmgmt/home/urls.py
@@ -2,9 +2,4 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.home,name='home')
-    '''
-    path('contact',views.contact_us,name="contactus"),
-    path('signup',views.signin,name='signup'),
-    path('signin',views.signup,name='signin')
-    '''
 ]
