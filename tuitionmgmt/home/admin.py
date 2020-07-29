38308dbaa334d87e422848d5228556a87978ec2d
Vinayak Jaiwant <vjaiwantx@gmail.com>
2020-07-29 13:11:12 +0530

    Added staff and subjects table

--- tuitionmgmt/home/admin.py
+++ tuitionmgmt/home/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Subject
+from .models import Subject,Staff
 
-from .forms import SubjectsAddForm
+from .forms import SubjectsAddForm,StaffsAddForm
 
 
 
@@ -13,4 +13,12 @@ class SubjectCreateAdmin(admin.ModelAdmin):
 	list_filter=['enrollment_name']
 	search_fields=['subject_name','enrollment_name']
 
+
+class StaffCreateAdmin(admin.ModelAdmin):
+	list_display=['first_name','last_name','staff_ph_no','addr','subject_code']
+	form=StaffsAddForm
+	list_filter=['subject_code']
+	search_fields=['first_name','last_name']	
+
 admin.site.register(Subject,SubjectCreateAdmin)
+admin.site.register(Staff,StaffCreateAdmin)
--- tuitionmgmt/home/forms.py
+++ tuitionmgmt/home/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Subject
+from .models import Subject,Staff
 
 class SubjectsAddForm(forms.ModelForm):
 	class Meta:
 		model=Subject()
 		fields=['subject_code','subject_name','enrollment_name']
+
+
+class StaffsAddForm(forms.ModelForm):
+	class Meta:
+		model=Staff()
+		fields=['first_name','last_name','staff_ph_no','addr','subject_code']		
