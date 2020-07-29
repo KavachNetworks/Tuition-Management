11855895b2a9dd590ffaffbf02292c6f59afd4a5
Vinayak Jaiwant <vjaiwantx@gmail.com>
2020-07-29 22:24:23 +0530

    Added Enrollment Table

--- tuitionmgmt/home/admin.py
+++ tuitionmgmt/home/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Subject,Staff,Student
+from .models import Subject,Staff,Student,Enrollment
 
-from .forms import SubjectsAddForm,StaffsAddForm,StudentsAddForm
+from .forms import SubjectsAddForm,StaffsAddForm,StudentsAddForm,EnrollmentsAddForm
 
-
+class EnrollmentCreateAdmin(admin.ModelAdmin):
+	list_display=['enrollment_name','subject_code']
+	form=EnrollmentsAddForm
 
 class SubjectCreateAdmin(admin.ModelAdmin):
-	list_display=['subject_code','subject_name','enrollment_name']
+	list_display=['subject_code','subject_name']
 	form=SubjectsAddForm
-	list_filter=['enrollment_name']
-	search_fields=['subject_name','enrollment_name']
+	search_fields=['subject_name']
 
 
 class StaffCreateAdmin(admin.ModelAdmin):
@@ -21,12 +22,12 @@ class StaffCreateAdmin(admin.ModelAdmin):
 	search_fields=['first_name','last_name']	
 
 
-
 class StudentCreateAdmin(admin.ModelAdmin):
-	list_display=['first_name','last_name','student_ph_no','addr']
+	list_display=['first_name','last_name','student_ph_no','addr','enrollment_name']
 	form=StudentsAddForm
 	search_fields=['first_name','last_name']
 
+admin.site.register(Enrollment,EnrollmentCreateAdmin)
 admin.site.register(Subject,SubjectCreateAdmin)
 admin.site.register(Staff,StaffCreateAdmin)
 admin.site.register(Student,StudentCreateAdmin)
